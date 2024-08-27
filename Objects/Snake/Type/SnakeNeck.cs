b2dfb03a1c2fa8c1d749664e98e40b65013977f5
chu123122 <yorushika123122@163.com>
2024-08-27 17:27:18 +0800

    测试之前

---
+++ Objects/Snake/Type/SnakeNeck.cs
@@ -0,0 +1,24 @@
+using Objects;
+using UnityEngine;
+using Utilities;
+
+public class SnakeNeck : SnakeBase
+{
+    private SnakeHead _head;
+    private void Start()
+    {
+        _head = GlobalGame.Instance.Head;
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        InitializePoints(new Point(x-1,y),new Point(x,y));
+        _head.SnakeHeadMoved += SnakeNeckMove;
+    }
+
+    private void SnakeNeckMove()
+    {
+        Debug.Log(gameObject.name + ":SnakeNeckMove");
+        Point target = _head.Points[0];
+        //SnakeMove(target,_head);
+        this.GetComponent<SpriteRenderer>().sprite = DetermineSprite(SnakeState);
+    }
+}
